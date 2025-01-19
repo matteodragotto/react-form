bc3363e507d2e29438967300ac440a9403a892b2
matteodragotto <matteodrag8@gmail.com>
2025-01-19 16:19:31 +0100

    [feat] array to register post titles

---
+++ data/postData.js
@@ -0,0 +1,14 @@
+const postTitles = [
+  "Exploring the Wonders of the Grand Canyon",
+  "A Journey Through the Streets of Tokyo",
+  "Discovering the Hidden Gems of Paris",
+  "Adventures in the Australian Outback",
+  "A Foodie's Guide to Italy",
+  "The Best Beaches in the Caribbean",
+  "Hiking the Trails of the Swiss Alps",
+  "Cultural Experiences in India",
+  "Road Trip Across the United States",
+  "A Safari Adventure in South Africa"
+];
+
+export default postTitles;
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
 import { useState } from "react"
 
+const postTitlesList = [
+  "Exploring the Wonders of the Grand Canyon",
+  "A Journey Through the Streets of Tokyo",
+  "Discovering the Hidden Gems of Paris",
+  "Adventures in the Australian Outback",
+  "A Foodie's Guide to Italy",
+  "The Best Beaches in the Caribbean",
+  "Hiking the Trails of the Swiss Alps",
+  "Cultural Experiences in India",
+  "Road Trip Across the United States",
+  "A Safari Adventure in South Africa"
+];
+
 const App = () => {
 
   const [postTitle, setPostTitle] = useState('')
@@ -24,8 +37,11 @@ const App = () => {
             <button className="btn btn-success my-3" type="submit"> Invia</button>
           </form>
 
-
-
+          <ul className="list-group">
+            {postTitlesList.map((title, index) => (
+              <li key={index} className="list-group-item">{title}</li>
+            ))}
+          </ul>
         </div>
       </div>
     </div>
