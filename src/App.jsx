1069eed9c64f55a9c468d0ee7d3f3b39ae086aca
matteodragotto <matteodrag8@gmail.com>
2025-01-19 16:13:42 +0100

    [feat] state form setup and event handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
+import { useState } from "react"
+
 const App = () => {
+
+  const [postTitle, setPostTitle] = useState('')
+  const submitHandler = (e) => {
+    e.preventDefault()
+    alert('Titolo registrato')
+  }
   return (
     <div className="container my-5">
       <div className="row">
         <h1>Inserisci qui il titolo dell'articolo per il tuo blog</h1>
         <div className="col-6 d-flex justify-content-center flex-column">
 
-          <form action="#">
-            <input type="text" className="form-control" />
+          <form action="#" onSubmit={submitHandler}>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Titolo"
+              value={postTitle}
+              onChange={((e) => setPostTitle(e.target.value))}
+            />
             <button className="btn btn-success my-3" type="submit"> Invia</button>
           </form>
 
